559d2dece69a52919c6ea3e1be323ba5dc950142
snowdream <snowdream@users.noreply.github.com>
2026-06-01 09:40:25 +0800

    test(service): disable zerolog globally in tests to fix Example tests output mismatch

---
+++ internal/service/main_test.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2026 SnowdreamTech. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package service_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rs/zerolog"
+)
+
+func TestMain(m *testing.M) {
+	// Disable logging during tests to prevent log output from interfering with Example test Output checks
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+	os.Exit(m.Run())
+}
